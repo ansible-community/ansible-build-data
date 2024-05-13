74736c08a3c44b2b94607d929b669c54efa02c8c
Felix Fontein <felix@fontein.de>
2024-05-13 20:18:07 +0200

    Add linting workflow with yamllint; fix linting issues (#396)
    * Add linting workflow with yamllint. Fix linting issues.
    
    * Use nox for running yamllint.

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,7 @@
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2023 Maxwell G <maxwell@gtmx.me
+
 from __future__ import annotations
 
 import nox
@@ -7,3 +11,9 @@ import nox
 def mkdocs(session: nox.Session):
     session.install("-r", "docs-requirements.txt")
     session.run("mkdocs", *(session.posargs or ["build"]))
+
+
+@nox.session
+def lint(session: nox.Session):
+    session.install("yamllint")
+    session.run("yamllint", ".")
